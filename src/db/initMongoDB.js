8f379addc98a6accaee859a38cd1906f5d6c0173
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2025-04-17 13:18:18 +0100

    correction

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -1,17 +1,18 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
+import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoDB = async () => {
-
-    const user = process.env.MONGODB_USER;
-    const url = process.env.MONGODB_URL;
-    const password = process.env.MONGODB_PASSWORD;
-    const db = process.env.MONGODB_DB;
-    try {
-        await mongoose.connect(`mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`);
-        console.log("Mongo connection successfully established!");
-    } catch (error) {
-        console.log(error.message);
-        throw error;
-    }
-}
+  const user = getEnvVar('MONGODB_USER');
+  const url = getEnvVar('MONGODB_URL');
+  const password = getEnvVar('MONGODB_PASSWORD');
+  const db = getEnvVar('MONGODB_DB');
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,8 @@
-import Contacts from "../db/models/Contacts.js"
+import Contacts from '../db/models/Contacts.js';
 
 const getAllContacts = async () => {
-const allContacts = await Contacts.find()
-return allContacts;
-}
+  const allContacts = await Contacts.find();
+  return allContacts;
+};
 
-export default getAllContacts
+export default getAllContacts;
